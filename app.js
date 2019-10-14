05315b0d8e517eea950ba332a598ac38dc094124
Marco Rosati <marcorosati_1@libero.it>
2019-10-14 14:27:19 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+var msg = process.env.APP_MSG;
+
+var response;
+
+app.get('/', function (req, res) {
+  response = 'Value in APP_MSG env var is'+ msg + '\n';
+  fs.readFile('opt/app-root/secure/myapp.sec', 'utf8', function(secerr, secdata){
+  if (secerr){
+   console.log(secerr+ '\n');
+   response += secerr+ '\n';
+  }
+  else {
+   response += 'The secret is =>'+ secdata +'\n';
+  }
+  res.send(response);
+});
+
+app.listen(8080, function () {
+  console.log('Server listening on port 8080!');
+});
