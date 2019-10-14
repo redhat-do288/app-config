cdbf0037eb28bc30d818668f308ff259e7d7d8d2
Marco Rosati <marcorosati_1@libero.it>
2019-10-14 14:34:45 -0400

    app.js

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ var response;
 app.get('/', function (req, res) {
   response = 'Value in APP_MSG env var is'+ msg + '\n';
   fs.readFile('opt/app-root/secure/myapp.sec', 'utf8', function(secerr, secdata){
-  if (secerr){
-   console.log(secerr+ '\n');
-   response += secerr+ '\n';
-  }
-  else {
-   response += 'The secret is =>'+ secdata +'\n';
-  }
-  res.send(response);
+	 if (secerr){
+  		 console.log(secerr+ '\n');
+  		 response += secerr+ '\n';
+ 	 }
+ 	 else {
+  		 response += 'The secret is =>'+ secdata +'\n';
+ 	 }
+ 	 res.send(response);
+	});
 });
-
 app.listen(8080, function () {
   console.log('Server listening on port 8080!');
 });
